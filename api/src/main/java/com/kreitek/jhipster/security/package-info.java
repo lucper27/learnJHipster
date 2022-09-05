38b99669db3678dfd823d599a7dec4ef71256b2a
Lucas Peruchena | NUNSYS <lucas.peruchena@nunsys.com>
2022-09-05 11:36:40 +0200

    Initial version of API learnjhipster generated by generator-jhipster@7.9.3

---
+++ api/src/main/java/com/kreitek/jhipster/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.kreitek.jhipster.security;
---
+++ api/src/test/java/com/kreitek/jhipster/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.kreitek.jhipster;
+
+import com.kreitek.jhipster.LearnjhipsterApp;
+import com.kreitek.jhipster.config.AsyncSyncConfiguration;
+import com.kreitek.jhipster.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { LearnjhipsterApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}

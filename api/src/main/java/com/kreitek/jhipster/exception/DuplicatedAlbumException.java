33a94a7b681911b0c1736d3aa9ec3deb05d0a070
NUNSYS\lucas.peruchena <lucas.peruchena@nunsys.com>
2023-10-24 07:57:22 +0200

    se borran los response status

--- api/src/main/java/com/kreitek/jhipster/exception/DuplicatedAlbumException.java
+++ api/src/main/java/com/kreitek/jhipster/exception/DuplicatedAlbumException.java
@@ -1,11 +1,6 @@
 package com.kreitek.jhipster.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.zalando.problem.Status;
-import org.zalando.problem.StatusType;
 
-@ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicatedAlbumException extends RuntimeException {
 
     public DuplicatedAlbumException(String message) {
